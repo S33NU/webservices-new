9869872d96e6fc37112d7b8842a7188aba4c8471
S33NU <62537245+S33NU@users.noreply.github.com>
2020-03-26 17:37:47 +0530

    Update metadata.py
    It shows lot of modifications, Don't worry there are formatted in the right order in editor by default.....
    I only add get_client_ip() method thats it

--- metadata/functions/metadata.py
+++ metadata/functions/metadata.py
@@ -3,64 +3,72 @@ import configparser
 import mysql.connector
 from mysql.connector import errorcode
 import http.client
+
+
 def getConfig():
     try:
         config = configparser.ConfigParser()
         config.read('D:\Production_files\config\webservicesconfig.ini')
         return config
     except Exception as e:
-        raise 
+        raise
+
 
 def configureLogging(logConfig):
     try:
-        logging.basicConfig(filename=logConfig['dir_path']+'webservice.log',
-                        level=logConfig.getint('level'), format=logConfig['format'], datefmt=logConfig['date_format'])
-        
+        logging.basicConfig(filename=logConfig['dir_path'] + 'webservice.log',
+                            level=logConfig.getint('level'), format=logConfig['format'],
+                            datefmt=logConfig['date_format'])
+
     except Exception as e:
-        raise   
+        raise
+
 
 def connectToDatabase():
     try:
-        config=getConfig()
+        config = getConfig()
         sqldb_config = config['sqldb_config']
         cnx = mysql.connector.connect(user=sqldb_config['user'], password=sqldb_config['password'],
-                              host=sqldb_config['host'],
-                              database=sqldb_config['database'])
+                                      host=sqldb_config['host'],
+                                      database=sqldb_config['database'])
         return cnx
     except Exception as err:
-        logging.error("Error in connecting to database "+str(err))
-        raise 
-    
+        logging.error("Error in connecting to database " + str(err))
+        raise
+
+
 def getOTP(mobile_number):
     try:
-        config=getConfig()
+        config = getConfig()
         otp_config = config['OTP_CONFIG']
         conn = http.client.HTTPSConnection("api.msg91.com")
 
-        headers = { 'content-type': "application/json" }
-        #%7B%22OTP%22%3A%2212390%22%7D
-        url = "/api/v5/otp?authkey="+otp_config['auth_key']+"&template_id="+otp_config['send_otp_template_id']+"&extra_param=&mobile="+mobile_number
-        conn.request("GET",url, headers=headers)
+        headers = {'content-type': "application/json"}
+        # %7B%22OTP%22%3A%2212390%22%7D
+        url = "/api/v5/otp?authkey=" + otp_config['auth_key'] + "&template_id=" + otp_config[
+            'send_otp_template_id'] + "&extra_param=&mobile=" + mobile_number
+        conn.request("GET", url, headers=headers)
 
         res = conn.getresponse()
         data = res.read()
 
         print(data.decode("utf-8"))
-    
+
     except Exception as err:
         logging.error("Error in getting OTP")
         raise
-    
-def verifyOTP(mobile_number,otp):
+
+
+def verifyOTP(mobile_number, otp):
     try:
-        config=getConfig()
+        config = getConfig()
         otp_config = config['OTP_CONFIG']
         conn = http.client.HTTPSConnection("api.msg91.com")
 
         payload = ""
 
-        url = "/api/v5/otp/verify?mobile="+mobile_number+"&otp="+otp+"&authkey="+otp_config['auth_key']       
-    
+        url = "/api/v5/otp/verify?mobile=" + mobile_number + "&otp=" + otp + "&authkey=" + otp_config['auth_key']
+
         conn.request("POST", url, payload)
 
         res = conn.getresponse()
@@ -68,4 +76,13 @@ def verifyOTP(mobile_number,otp):
 
         print(data.decode("utf-8"))
     except Exception as e:
-        logging.error("Error in verifyting OTP "+str(e))
+        logging.error("Error in verifyting OTP " + str(e))
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
