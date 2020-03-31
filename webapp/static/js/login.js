0208c617f9b9acbb5bf6e244930912bfb1e1787a
S33NU <62537245+S33NU@users.noreply.github.com>
2020-03-31 22:35:17 +0530

    update of login.js

--- webapp/static/js/login.js
+++ webapp/static/js/login.js
@@ -2,6 +2,7 @@
     $("#existingUserLogin").hide();
     $("#newUserLogin").hide();
     $("#loginSubmit").hide();
+    $("#otpMessage").hide();
     $('#userMobileNumber').parsley().on('field:success', function() {
         var phonenumber =  $('#userMobileNumber').val();
         dataObj={
@@ -18,15 +19,18 @@
                 
                 if(data){
                     console.log("old");
+                    $("#otpMessage").hide();
                     $("#newUserLogin").hide();
-                    $("#existingUserLogin").show();  
+                    $("#existingUserLogin").show();
                 }else{
                     console.log("new");
                     $("#newUserLogin").show();
+                    $("#otpMessage").show();
                     $("#otpMessage").html("OTP sent to "+phonenumber);
                     $("#existingUserLogin").hide();  
                 }
             }else if(res.statusCode == 1){
+            console.log(res);
                 $('.invalid-form-error-message')
                     .html('Internal Server Error, try later')
                     .toggleClass('filled', true);
@@ -38,6 +42,7 @@
     $('#userMobileNumber').parsley().on('field:error', function() {
         $("#existingUserLogin").hide();
         $("#newUserLogin").hide();
+        $("#otpMessage").hide();
         $("#loginSubmit").hide();
         $('.invalid-form-error-message')
         .html('')
@@ -112,6 +117,7 @@ function verifyUserCredentials(){
                     .html('Invalid Credentials')
                     .toggleClass('filled', true);
                 }else if (statusCode == 1){
+                console.log(res);
                     $('.invalid-form-error-message')
                     .html('Internal Server Error, try later')
                     .toggleClass('filled', true);
