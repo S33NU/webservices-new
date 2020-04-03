307dd6ef812826ac40828ed89a5f85e764700577
S33NU <62537245+S33NU@users.noreply.github.com>
2020-04-03 10:46:11 +0530

    Creation of PersonalProfile model at models.py

--- personal_profile/models.py
+++ personal_profile/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class PersonalProfile(models.Model):
+    registeredmobile = models.CharField(max_length=225)
+    firstname = models.CharField(max_length=225)
+    lastname = models.CharField(max_length=225)
+    email = models.CharField(max_length=225)
+    age = models.CharField(max_length=225)
+    marriedstatus = models.CharField(max_length=225)
+    addr1 = models.CharField(max_length=225)
+    addr2 = models.CharField(max_length=225)
+    addr3 = models.CharField(max_length=225)
+    addr4 = models.CharField(max_length=225)
+    occupation = models.CharField(max_length=225)
